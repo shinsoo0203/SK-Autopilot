841ba697f7ad6bad4f079ad334b171bc9dbe0ba7
DWKE <lkeun1119@gmail.com>
2019-09-28 05:06:11 +0900

    connection

--- src/sk_autopilot/src/mission.cpp
+++ src/sk_autopilot/src/mission.cpp
@@ -18,18 +18,27 @@ class Mission {
 protected:
     ros::NodeHandle nh;
     ros::Subscriber reached_sub;
+    ros::Subscriber state_sub;
     ros::Publisher mission_perform_pub;
     bool mission_start;
     std_msgs::Bool mission_perform;
     int wp_seq;
 
+
 public:
+    mavros_msgs::State current_state;
     Mission() {
       reached_sub = nh.subscribe
               ("mavros/mission/reached",100,&Mission::reached_cb,this);
-      mission_perform_pub = nh.advertise<std_msgs::Bool>("sk/mission_perform",10);
+      mission_perform_pub = nh.advertise<std_msgs::Bool>
+          ("sk/mission_perform",10);
+      state_sub = nh.subscribe<mavros_msgs::State>
+                  ("mavros/state", 10, &Mission::state_cb,this);
       mission_start=false;
       mission_perform.data=false;
+    }
+    void state_cb(const mavros_msgs::State::ConstPtr& msg){
+        current_state = *msg;
     }
     void reached_cb(const mavros_msgs::WaypointReached::ConstPtr& msg) {
         wp_seq=msg->wp_seq;
@@ -72,11 +81,13 @@ int main(int argc, char** argv) {
       ROS_INFO("Wiring connect fail");
     }
 
+
     ROS_INFO("Waiting for mission start");
 
     while(ros::ok() && !mission.missionStarted()) {
       ros::spinOnce();
       rate.sleep();
+      ROS_INFO("connection : %d",mission.current_state.connected);
     }
     stage++;
 
